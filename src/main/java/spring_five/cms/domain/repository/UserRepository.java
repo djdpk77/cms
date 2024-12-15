f91add903c562b35282fcb86a35eeb125a38b1e9
Durga Prasad Kar <durgaprasadkar1998.dp@gmail.com>
2024-12-15 17:08:45 +0530

    Added Repo layer
     - Added AbstractRepository to store a list of objects in memory instead of DB
     - Also added UserRepository and CategoryRepository which extends AbstractRepository

---
+++ src/main/java/spring_five/cms/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package spring_five.cms.domain.repository;
+
+import org.springframework.stereotype.Service;
+import spring_five.cms.domain.models.User;
+
+@Service
+public class UserRepository extends AbstractRepository<User>{
+}
